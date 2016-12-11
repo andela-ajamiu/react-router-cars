c7207e1aa85ba42f10b726893d82db8cd5883c90
Emjay <abdulmujeeb.jamiu@andela.com>
2016-12-11 12:49:41 +0100

    implement nested routes on cars

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,60 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 // Import routing components
-import {Router, Route, IndexRoute, browserHistory, Link } from 'react-router';
+import {Router, Route, browserHistory, Link } from 'react-router';
+
+
+// Static data
+const data = [
+    {
+        id: 1,
+        name: 'Honda Accord Crosstour',
+        year: '2010',
+        model: 'Accord Crosstour',
+        make: 'Honda',
+        media: 'http://media.ed.edmunds-media.com/honda/accord-crosstour/2010/oem/2010_honda_accord-crosstour_4dr-hatchback_ex-l_fq_oem_4_500.jpg',
+        price: '$16,811'
+
+    },
+    {
+        id: 2,
+        name: 'Mercedes-Benz AMG GT Coupe',
+        year: '2016',
+        model: 'AMG',
+        make: 'Mercedes Benz',
+        media: 'http://media.ed.edmunds-media.com/mercedes-benz/amg-gt/2016/oem/2016_mercedes-benz_amg-gt_coupe_s_fq_oem_1_717.jpg',
+        price: '$138,157'
+
+    },
+    {
+        id: 3,
+        name: 'BMW X6 SUV',
+        year: '2016',
+        model: 'X6',
+        make: 'BMW',
+        media: 'http://media.ed.edmunds-media.com/bmw/x6/2016/oem/2016_bmw_x6_4dr-suv_xdrive50i_fq_oem_1_717.jpg',
+        price: '$68,999'
+    },
+    {
+        id: 4,
+        name: 'Ford Edge SUV',
+        year: '2016',
+        model: 'Edge',
+        make: 'Ford',
+        media: 'http://media.ed.edmunds-media.com/ford/edge/2016/oem/2016_ford_edge_4dr-suv_sport_fq_oem_6_717.jpg',
+        price: '$36,275'
+    },
+    {
+        id: 5,
+        name: 'Dodge Viper Coupe',
+        year: '2017',
+        model: 'Viper',
+        make: 'Dodge',
+        media: 'http://media.ed.edmunds-media.com/dodge/viper/2017/oem/2017_dodge_viper_coupe_acr_fq_oem_3_717.jpg',
+        price: '$123,890'
+    }
+];
+
 
 class Main extends Component {
     render() {
@@ -38,72 +91,23 @@ class Home extends Component {
 
 // More components
 class Car extends Component {
-    constructor() {
-        super();
-        this.state = {
-            cars: []
-        }
-    };
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         cars: []
+    //     }
+    // };
 
-    componentWillMount(){
-        // Static data
-        const data = [
-            {
-                id: 1,
-                name: 'Honda Accord Crosstour',
-                year: '2010',
-                model: 'Accord Crosstour',
-                make: 'Honda',
-                media: 'http://media.ed.edmunds-media.com/honda/accord-crosstour/2010/oem/2010_honda_accord-crosstour_4dr-hatchback_ex-l_fq_oem_4_500.jpg',
-                price: '$16,811'
-
-            },
-            {
-                id: 2,
-                name: 'Mercedes-Benz AMG GT Coupe',
-                year: '2016',
-                model: 'AMG',
-                make: 'Mercedes Benz',
-                media: 'http://media.ed.edmunds-media.com/mercedes-benz/amg-gt/2016/oem/2016_mercedes-benz_amg-gt_coupe_s_fq_oem_1_717.jpg',
-                price: '$138,157'
-
-            },
-            {
-                id: 3,
-                name: 'BMW X6 SUV',
-                year: '2016',
-                model: 'X6',
-                make: 'BMW',
-                media: 'http://media.ed.edmunds-media.com/bmw/x6/2016/oem/2016_bmw_x6_4dr-suv_xdrive50i_fq_oem_1_717.jpg',
-                price: '$68,999'
-            },
-            {
-                id: 4,
-                name: 'Ford Edge SUV',
-                year: '2016',
-                model: 'Edge',
-                make: 'Ford',
-                media: 'http://media.ed.edmunds-media.com/ford/edge/2016/oem/2016_ford_edge_4dr-suv_sport_fq_oem_6_717.jpg',
-                price: '$36,275'
-            },
-            {
-                id: 5,
-                name: 'Dodge Viper Coupe',
-                year: '2017',
-                model: 'Viper',
-                make: 'Dodge',
-                media: 'http://media.ed.edmunds-media.com/dodge/viper/2017/oem/2017_dodge_viper_coupe_acr_fq_oem_3_717.jpg',
-                price: '$123,890'
-            }
-        ];
-        // Update state
-        this.setState({cars: data});
-    }
+    // componentWillMount(){
+    //     // Update state
+    //     this.setState({cars: data});
+    // }
 
     render(){
+        const cars = this.props.route.data;
 
-        const carNode = this.state.cars.map( (car) => 
-            <a href="#" className="list-group-item" key={car.id}>{car.name}</a>
+        const carNode = cars.map( (car) => 
+            <Link to={"/cars/" + car.id} className="list-group-item" key={car.id}>{car.name}</Link>
         );
 
         // console.log(carNode)
@@ -121,9 +125,45 @@ class Car extends Component {
 
 
 class CarDetail extends Component {
+    _localHandleClick() {
+        browserHistory.push('/cars');
+    };
+
     render(){
-        return (<h1>{this.props.params.id}</h1>);
-    }
+        const cars = this.props.route.data;
+
+        const car_id = this.props.params.id;
+
+        const car = cars.filter( (car) => {
+            if (car.id == car_id) {
+                return car;
+            }
+        })
+
+        return(
+            <div>
+                <div>
+                    <button className="btn btn-default" onClick={this._localHandleClick}>Go to Cars</button>
+                </div>
+                <h1>{car[0].name}</h1>
+                <div className="row">
+                    <div className="col-sm-6 col-md-4">
+                        <div className="thumbnail">
+                            <img src={car[0].media} alt={car[0].name} />
+                        </div>
+                    </div>
+                    <div className="col-sm-6 col-md-4">
+                       <ul>
+                           <li><strong>Model</strong>: {car[0].model}</li>
+                           <li><strong>Make</strong>: {car[0].make}</li>
+                           <li><strong>Year</strong>: {car[0].year}</li>
+                           <li><strong>Price</strong>: {car[0].price}</li>
+                       </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    };
 }
 
 
@@ -137,11 +177,11 @@ class About extends Component {
 
 
 render(
-    <Router history={browserHistory} >
-        <Route path="/" component={Main}>
-            <IndexRoute component={Home}/>
-            <Route path="/cars" component={Car}/>
-            <Route path="/cars/:id" component={CarDetail}/>
+    <Router history={browserHistory}>
+        <Route component={Main}>
+            <Route path="/" component={Home}/>
+            <Route path="/cars" component={Car} data={data}/>
+            <Route path="/cars/:id" component={CarDetail} data={data}/>
             <Route path="/about" component={About}/>
         </Route>
     </Router>,
