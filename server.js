f04fe96f173e9ef18f7df5a3db4c372668bbc34e
Emjay <abdulmujeeb.jamiu@andela.com>
2017-08-30 09:53:13 +0100

    modify server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+// const env = process.env.NODE_ENV = 'development';
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// process.env.NODE_ENV
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,5 +14,15 @@ app.get('*', function(req, res){
     res.sendFile(path.resolve(__dirname + '/public/index.html'))
 });
 
-app.listen(PORT);
-console.log(`App started on ${PORT}...`);
+var server = app.listen(PORT, () => {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('App started on http://%s:%s', host, port)
+});
+
+// var server = app.listen(PORT, function () {
+//     var port = server.address().port;
+//     require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+//         console.log('Example app listening at http://%s:%s', add, port);
+//     })
+// });
